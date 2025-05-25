ee7f55ff2d58ac3150a719d9d8e30dae5d338207
David_M <titans156@gmail.com>
2025-05-25 22:15:30 +0300

    Removed values less than 0

--- find_and_elim_missing_values.py
+++ find_and_elim_missing_values.py
@@ -1,8 +1,7 @@
 def elim_missing_values(data_frame):
     check_fav_win_cond = data_frame["Favourite_Win_Condition"] == "N.A."
     check_win_probability_greater_than_100 = data_frame["Win_Probability"] > 100
-    check_win_probability_less_than_0 = data_frame["Win_Probability"] < 0
-    check_all = check_fav_win_cond | check_win_probability_greater_than_100 | check_win_probability_less_than_0
+    check_all = check_fav_win_cond | check_win_probability_greater_than_100
     nr_missing_values = check_all.sum()
 
     data_frame = data_frame[~check_all]
