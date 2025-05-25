53ecd934e2740e99124824eb42b2c3e939e28905
David_M <titans156@gmail.com>
2025-05-25 18:40:56 +0300

    Added outliers and heatmap

--- graphs.py
+++ graphs.py
@@ -29,3 +29,29 @@ def get_countplot(data_frame, choice):
             plt.savefig(f"{i}_test.png")
         plt.tight_layout()
         plt.close()
+
+def get_outliers(data_frame, choice):
+    numerical_charact = ["Trophy_Count", "Current_Win_Streak", "Win_Percentage", "Average_Elixir_Cost", "Win_Condition_Level", "Win_Probability"]
+    for i in numerical_charact:
+        plt.figure(figsize = (10, 5))
+        sns.boxplot(data = data_frame, y = i)
+        plt.title(f"{i}")
+        if choice == 0:
+            plt.savefig(f"{i}_train.jpeg")
+        elif choice == 1:
+            plt.savefig(f"{i}_test.jpeg")
+        plt.tight_layout()
+        plt.close()
+
+def get_heatmap(data_frame, choice):
+    numerical_charact = ["Trophy_Count", "Current_Win_Streak", "Win_Percentage", "Average_Elixir_Cost", "Win_Condition_Level", "Win_Probability"]
+    cm = data_frame.corr(numeric_only = True)
+    plt.figure(figsize = (10, 5))
+    sns.heatmap(cm, annot = True, fmt = '.2f', cmap = 'Blues')
+    plt.title("Matricea de confuzie (MNIST)")
+    plt.tight_layout()
+    if choice == 0:
+        plt.savefig("Heatmap_train.jpg")
+    elif choice == 1:
+        plt.savefig("Heatmap_test.jpg")
+    plt.close()
